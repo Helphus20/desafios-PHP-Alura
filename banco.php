400355ca919662c5a51cd03026f1ad0e8337b804
unknown <Felipe da Silva>
2023-05-20 20:03:34 -0300

    separando os arquivos de funcoes

--- banco.php
+++ banco.php
@@ -1,5 +1,6 @@
 <?php 
 
+require 'funcoes.php';
 $contasCorrentes = [
     '123.456.789-10' => [
         'titular' => 'Felipe', 
@@ -18,43 +19,6 @@ $contasCorrentes = [
 $numId = '123.345.678-15';
 $saque = 500;
 $deposito = 198;
-
-function escreva($mensagem): void
-{
-    echo $mensagem . PHP_EOL;
-}
-function validaSaldo($saque, $saldo)
-{
-    if($saque > $saldo){
-        return 1;
-    }
-}
-function saldoInsuficiente($nome, $saldo): void
-{
-    escreva(mensagem: "$nome não tem saldo suficiente para esta operação, solicitação CANCELADA");
-    escreva(mensagem: "Saldo: R$ $saldo");
-}
-function saque($saldo, $saque, $nome, $numId, $conta): void
-{
-    $conta = $saldo - $saque;
-    escreva(mensagem: "$nome seu cpf é $numId");
-    escreva(mensagem: "seu saldo é $conta");
-}
-
-function validaDeposito ($deposito)
-{
-    if($deposito < 0)
-        return 1;
-}
-function deposito($conta, $deposito)
-{
-    return $conta += $deposito;
-}
-function mostraSaldo($conta): void
-{
-    echo "Seu saldo é: R$ $conta" . PHP_EOL;
-}
-
 
 escreva(mensagem: "Saldo atual de  {$contasCorrentes[$numId]['titular']}: R$ {$contasCorrentes[$numId]['saldo']}");
 escreva(mensagem: "Fazendo um depósito no valor de R$ $deposito");
---
+++ funcoes.php
@@ -0,0 +1,37 @@
+<?php
+function escreva($mensagem): void
+{
+    echo $mensagem . PHP_EOL;
+}
+function validaSaldo($saque, $saldo)
+{
+    if($saque > $saldo){
+        return 1;
+    }
+}
+function saldoInsuficiente($nome, $saldo): void
+{
+    escreva(mensagem: "$nome não tem saldo suficiente para esta operação, solicitação CANCELADA");
+    escreva(mensagem: "Saldo: R$ $saldo");
+}
+function saque($saldo, $saque, $nome, $numId, $conta): void
+{
+    $conta = $saldo - $saque;
+    escreva(mensagem: "$nome seu cpf é $numId");
+    escreva(mensagem: "seu saldo é $conta");
+}
+
+function validaDeposito ($deposito)
+{
+    if($deposito < 0)
+        return 1;
+}
+function deposito($conta, $deposito)
+{
+    return $conta += $deposito;
+}
+function mostraSaldo($conta): void
+{
+    echo "Seu saldo é: R$ $conta" . PHP_EOL;
+}
+
